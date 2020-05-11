c56e58df4310b35eb9c82eed2b8e2714cd733749
Arslan Sajid <arslansajid951@gmail.com>
2020-05-12 02:28:10 +0500

    Added routes

--- src/static/_routes.js
+++ src/static/_routes.js
@@ -2,8 +2,8 @@ const routes = {
   '/': 'Home',
 
   '/users': 'Users',
-  '/users/user_form': 'User Form',
-  '/users/edit_user': 'Edit User',
+  '/users/user-form': 'User Form',
+  '/users/edit-user': 'Edit User',
 
   '/collection': 'Collection',
   '/collection/collection-form': 'Collection Form',
@@ -24,6 +24,14 @@ const routes = {
   '/events': 'Events',
   '/events/event-form': 'Event Form',
   '/events/edit-event': 'Edit Event',
+
+  '/privacy-policy': 'Privacy Policy',
+  '/privacy-policy/privacy-policy-form': 'Privacy Policy Form',
+  '/privacy-policy/edit-privacy-policy': 'Edit Privacy Policy',
+
+  '/terms-service': 'Terms Service',
+  '/terms-service/terms-service-form': 'Terms Service Form',
+  '/terms-service/edit-terms-service': 'Edit Terms Service',
 
 };
 export default routes;
