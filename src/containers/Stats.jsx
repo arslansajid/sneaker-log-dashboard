fae9a1238401b363d82f8253c33f5e33d9724e0d
Arslan Sajid <arslansajid951@gmail.com>
2020-05-22 21:36:50 +0500

    firebase intrgration in stats module

--- src/containers/Stats.jsx
+++ src/containers/Stats.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Cookie from 'js-cookie';
-import { API_END_POINT } from '../config';
-const token = Cookie.get('sneakerlog_access_token');
+
+import {getBrands} from "../backend/services/brandService"
+import {getEvents} from "../backend/services/eventService"
+import {getUsers} from "../backend/services/usersService"
+import {getSneakersReleaseDates} from "../backend/services/sneakerReleaseService"
 
 // import {Pagination} from 'react-bootstrap';
 // import LineChart from '../components/LineChart'
@@ -16,37 +17,42 @@ export default class Area extends React.Component {
     super(props);
 
     this.state = {
-      exercises: [],
-      programs: [],
+      brands: [],
+      events: [],
+      users: [],
+      releaseDates: []
     }
   }
 
-  // componentWillMount() {
-  //     axios.get(`${API_END_POINT}/api/v1/exercise`)
-  //     .then(response => {
-  //       this.setState({
-  //         exercises: response.data.data,
-  //         loading: false,
-  //         responseMessage: 'No Exercise Found'
-  //       })
-  //     })
-  //     .catch(err => {
-  //       console.log("error fetching data");
-  //     })
-  //     axios.get(`${API_END_POINT}/api/v1/program`)
-  //     .then(response => {
-  //       this.setState({
-  //         programs: response.data.program_set,
-  //         responseMessage: 'No Programs Found...'
-  //       })
-  //     })
-  //     .catch(err => {
-  //       console.log("error fetching data");
-  //     })
-  // }
+  componentWillMount() {
+      getUsers()
+      .then(response => {
+        this.setState({
+          users: response,
+        })
+      })
+      getBrands()
+      .then(response => {
+        this.setState({
+          brands: response,
+        })
+      })
+      getEvents()
+      .then(response => {
+        this.setState({
+          events: response,
+        })
+      })
+      getSneakersReleaseDates()
+      .then(response => {
+        this.setState({
+          releaseDates: response,
+        })
+      })
+  }
 
   render() {
-    const { exercises, programs } = this.state;
+    const { users, brands, events, releaseDates } = this.state;
     return (
       <div className="row">
         <div className="col-12">
@@ -61,12 +67,11 @@ export default class Area extends React.Component {
             <div className = 'row space-1'>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Total Users</h3>
-                {/* <h5>{programs.length ? programs.length : "Fetching programs..."}</h5> */}
-                <h5>0</h5>
+                <h5>{users.length ? users.length : "Fetching users..."}</h5>
               </div>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Total Brands</h3>
-                <h5>0</h5>
+                <h5>{brands.length ? brands.length : "Fetching brands..."}</h5>
               </div>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Total News Articles</h3>
@@ -74,7 +79,7 @@ export default class Area extends React.Component {
               </div>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Total Events</h3>
-                <h5>0</h5>
+                <h5>{events.length ? events.length : "Fetching events..."}</h5>
               </div>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Total Products</h3>
@@ -82,7 +87,7 @@ export default class Area extends React.Component {
               </div>
               <div className='col-sm-6 my-3'>
                 <h3 className='space-1'>Sneaker Releases</h3>
-                <h5>0</h5>
+                <h5>{releaseDates.length ? releaseDates.length : "Fetching elease dates..."}</h5>
               </div>
             </div>
         </div>
