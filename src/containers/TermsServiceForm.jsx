6da5d9841dc50e0d8779bb2f04c1094037e727e2
Arslan Sajid <arslansajid951@gmail.com>
2020-05-22 21:36:14 +0500

    firebase intrgration in terms module

--- src/containers/TermsServiceForm.jsx
+++ src/containers/TermsServiceForm.jsx
@@ -1,193 +1,64 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
 import RichTextEditor from 'react-rte';
 import { Button } from 'reactstrap';
-import { API_END_POINT } from '../config';
-import {toolbarConfig} from "../static/_textEditor";
-import Cookie from 'js-cookie';
-
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
+import { toolbarConfig } from "../static/_textEditor";
+import { getTerms, updateTerms } from "../backend/services/termsService"
 
 export default class TermsServiceForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      terms: [],
       loading: false,
-      termsService: {
-        name: '',
-      },
       description: RichTextEditor.createEmptyValue(),
     };
-    
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.postTermsService = this.postTermsService.bind(this);
   }
 
   componentDidMount() {
-    const { match } = this.props;
-    if (match.params.termsServiceId) {
-      axios.get(`${API_END_POINT}/api/v1/termsService/${match.params.termsServiceId}`)
+    getTerms()
       .then((response) => {
-        let data  = response.data.termsService;
-        data["video_files"] = [];
+        console.log(response);
         this.setState({
-          termsService: data,
-        }, () => {
-          const {termsService} = this.state;
-          this.getWorkoutDayById(termsService.workout_day_id);
-          if(termsService.videos_url === null) {
-            termsService.video_files = [];
-            this.setState({ termsService })
-          } else {
-            this.setState({videoInputCount: termsService.videos_url.length })
-          }
+          terms: response,
+          description: RichTextEditor.createValueFromString(response[0].termsOfService, 'html'),
         });
       });
-    }
-    if (match.params.dayId) {
-      this.getWorkoutDayById(match.params.dayId)
-    } else {
-      this.getWorkoutDays();
-    }
   }
 
-  getWorkoutDays = () => {
-    axios.get(`${API_END_POINT}/api/v1/workout_days`)
-    .then(response => {
-      this.setState({
-        workoutDays: response.data.data,
-        responseMessage: 'No Workout Days Found...'
-      })
-    })
-  }
-
-  getWorkoutDayById = (workoutDayId) => {
-    axios.get(`${API_END_POINT}/api/v1/workout_days/${workoutDayId}`)
-    .then((response) => {
-      this.setState({
-        workoutDay: response.data.data,
-      });
-    });
-  }
-
-  setWorkoutDay(selectedWorkoutDay) {
-    this.setState(prevState => ({
-      workoutDay: selectedWorkoutDay,
-      termsService: {
-        ...prevState.termsService,
-        workout_day_id: !!selectedWorkoutDay ? selectedWorkoutDay.id : "",
-      },
-    }));
-  }
-
-  setCity(selectedCity) {
-    this.setState(prevState => ({
-      city: selectedCity,
-      termsService: {
-        ...prevState.termsService,
-        city_id: selectedCity.ID,
-      },
-    }));
-  }
-
-  setDescription(description) {
-    const { termsService } = this.state;
-    termsService.description = description.toString('html');
+  setDescription = (description) => {
+    const { terms } = this.state;
+    terms[0].termsOfService = description.toString('html');
     this.setState({
-      termsService,
+      terms,
       description,
     });
   }
 
-  handleInputChange(event) {
-    const { value, name } = event.target;
-
-    const { termsService } = this.state;
-    termsService[name] = value;
-    this.setState({ termsService });
-  }
-
-  handleVideoURLChange = (event, index) => {
-    const { name } = event.target;
-    const { termsService } = this.state;
-    termsService[name][index] = event.target.files[0];
-    this.setState({ termsService });
-  }
-
-  postTermsService(event) {
+  postTermsService = (event) => {
     event.preventDefault();
-    const { match, history } = this.props;
-    const { loading, termsService } = this.state;
+    const { loading, description, terms } = this.state;
     if (!loading) {
       const fd = new FormData();
 
-      // let videosArray = [];
-      // for (let index = 0; index < termsService.video_files.length; index += 1) {
-      //   videosArray.push(termsService.video_files[index]);
-      // }
-      if(!!termsService.video_files && termsService.video_files.length > 0) {
-        termsService.video_files.forEach((video, index) => {
-          fd.append(`video_files[${index}]`, video);
-        });
-        delete termsService["video_files"];
-      }
-
-      Object.keys(termsService).forEach((eachState) => {
-        fd.append(`${eachState}`, termsService[eachState]);
-      })
-
-      
-
       this.setState({ loading: true });
-      if (match.params.termsServiceId) {
-        axios.put(`${API_END_POINT}/api/v1/termsService/${match.params.termsServiceId}`, fd)
-          .then((response) => {
-            if (response.data && response.data.status && response.status === 200) {
-              window.alert("Updated !");
-              this.setState({ loading: false });
-            } else {
-              window.alert('ERROR UPDATING !')
-              this.setState({ loading: false });
-            }
-          })
-          .catch((err) => {
-            window.alert('ERROR UPDATING !')
-            this.setState({ loading: false });
-          })
-      }
-      else {
-        axios.post(`${API_END_POINT}/api/v1/termsService`, fd)
-          .then((response) => {
-            if (response.data && response.data.status && response.status === 200) {
-              window.alert("SUCCESS !");
-              this.setState({ loading: false });
-            } else {
-              window.alert('ERROR SAVING !')
-              this.setState({ loading: false });
-            }
-          })
-          .catch((err) => {
-            window.alert('ERROR SAVING !')
-            this.setState({ loading: false });
-          })
-      }
+      let cloneObject = Object.assign({}, terms[0])
+      updateTerms(terms[0].uuid, cloneObject)
+        .then((response) => {
+          window.alert("Updated !");
+          this.setState({ loading: false });
+        })
+        .catch((err) => {
+          console.log(err)
+          window.alert('ERROR UPDATING !')
+          this.setState({ loading: false });
+        }
+        )
     }
-  }
-
-  handleFile = (event) => {
-    this.setState({
-      profile_picture: event.target.files.length ? event.target.files[0] : '',
-    });
   }
 
   render() {
     console.log(this.state);
-    const {
-      termsService,
-      description
-    } = this.state;
+    const { description} = this.state;
     return (
       <div className="row animated fadeIn">
         <div className="col-12">
@@ -216,22 +87,6 @@ export default class TermsServiceForm extends React.Component {
                         }}
                       />
                     </div>
-
-                    {/* <div className="form-group row">
-                      <label className="control-label col-md-3 col-sm-3">Workout Day</label>
-                      <div className="col-md-6 col-sm-6">
-                      <Select
-                        onChange={(val) => this.setWorkoutDay(val)}
-                        options={workoutDays}
-                        placeholder="Select workout day"
-                        value={workoutDay}
-                        valueKey="id"
-                        labelKey="name"
-                        isClearable={false}
-                        disabled={workoutDaySelected}
-                      />
-                      </div>
-                    </div> */}
 
                     <div className="ln_solid" />
                     <div className="form-group row">
