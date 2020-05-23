eb4540c9f6cc590e43ccca48bc62259b304e2f56
Arslan Sajid <arslansajid951@gmail.com>
2020-05-23 05:17:04 +0500

    delete user snackbar

--- src/containers/Users.jsx
+++ src/containers/Users.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {Pagination} from 'react-bootstrap';
-import {getUsers, deleteUser} from "../backend/services/usersService";
+import { Pagination } from 'react-bootstrap';
+import { getUsers, deleteUser } from "../backend/services/usersService";
 // import {signInWithEmail} from "../backend/services/authService";
+import SnackBar from "../components/SnackBar";
 
 import { API_END_POINT } from '../config';
 import Cookie from 'js-cookie';
@@ -20,7 +21,10 @@ export default class Users extends React.Component {
       pages: 1,
       q: '',
       loading: false,
-      responseMessage: 'Loading Users...'
+      responseMessage: 'Loading Users...',
+      showSnackBar: false,
+      snackBarMessage: "",
+      snackBarVariant: "success"
     }
   }
   componentWillMount() {
@@ -35,7 +39,7 @@ export default class Users extends React.Component {
   }
 
   fetchUsers = () => {
-      getUsers()
+    getUsers()
       .then(response => {
         console.log("############", response)
         this.setState({
@@ -55,19 +59,30 @@ export default class Users extends React.Component {
   }
 
   removeUser(userId, index) {
-    if(confirm("Are you sure you want to delete this user?")) {
+    if (confirm("Are you sure you want to delete this user?")) {
       deleteUser(userId)
         .then(response => {
           const users = this.state.users.slice();
           users.splice(index, 1);
-          this.setState({ users });
-          window.alert("User deleted successfully")
-        });
+          this.setState({
+            users,
+            showSnackBar: true,
+            snackBarMessage: "User deleted successfully",
+            snackBarVariant: "success",
+          });
+        })
+        .catch(() => {
+          this.setState({
+            showSnackBar: true,
+            snackBarMessage: "Error deleting user",
+            snackBarVariant: "error",
+          });
+        })
     }
   }
 
   handleSelect(page) {
-    axios.get(`/api/area?offset=${(page-1)*10}`)
+    axios.get(`/api/area?offset=${(page - 1) * 10}`)
       .then(response => {
         this.setState({
           areas: response.data.items,
@@ -82,40 +97,55 @@ export default class Users extends React.Component {
         this.setState({
           areas: response.data.items,
           activePage: 1,
-          pages: Math.ceil(response.data.total/10)
+          pages: Math.ceil(response.data.total / 10)
         })
       })
   }
 
   handleSearch() {
     const { q } = this.state;
-    if(q.length) {
-      this.setState({loading: true, users: [], responseMessage: 'Loading Users...'})
-      axios.get(`${API_END_POINT}/api/users/search`, {params: {"searchWord": this.state.q}, headers: {"auth-token": token}})
-      .then((response) => {
-        this.setState({
-          users: response.data.searchedItems,
-          loading: false,
-          responseMessage: 'No Users Found...'
+    if (q.length) {
+      this.setState({ loading: true, users: [], responseMessage: 'Loading Users...' })
+      axios.get(`${API_END_POINT}/api/users/search`, { params: { "searchWord": this.state.q }, headers: { "auth-token": token } })
+        .then((response) => {
+          this.setState({
+            users: response.data.searchedItems,
+            loading: false,
+            responseMessage: 'No Users Found...'
+          })
         })
-      })
-      .catch(() => {
-        this.setState({
-          loading: false,
-          responseMessage: 'No Users Found...'
+        .catch(() => {
+          this.setState({
+            loading: false,
+            responseMessage: 'No Users Found...'
+          })
         })
-      })
     }
+  }
+
+  closeSnackBar = () => {
+    this.setState({ showSnackBar: false })
   }
 
   render() {
     // console.log(this.state);
-    const {loading, users, responseMessage} = this.state; 
+    const { loading, users, responseMessage,
+      showSnackBar,
+      snackBarMessage,
+      snackBarVariant } = this.state;
     return (
       <div className="row animated fadeIn">
+        {showSnackBar && (
+          <SnackBar
+            open={showSnackBar}
+            message={snackBarMessage}
+            variant={snackBarVariant}
+            onClose={() => this.closeSnackBar()}
+          />
+        )}
         <div className="col-12">
           <div className="row space-1">
-          <div className="col-sm-4">
+            <div className="col-sm-4">
               <h3>List of Users</h3>
             </div>
             <div className="col-sm-4">
@@ -142,9 +172,9 @@ export default class Users extends React.Component {
               </div> */}
             </div>
             <div className="col-sm-4 pull-right mobile-space">
-                <Link to='/users/user-form'>
-                  <button type="button" className="btn btn-success">Add new User</button>
-                </Link>
+              <Link to='/users/user-form'>
+                <button type="button" className="btn btn-success">Add new User</button>
+              </Link>
             </div>
           </div>
           <div className="table-responsive">
@@ -165,33 +195,33 @@ export default class Users extends React.Component {
               </thead>
               <tbody>
                 {this.state.users && this.state.users.length >= 1 ?
-                this.state.users.map((user, index) => (
-                  <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{<img style={{height: '50px', width: '50px'}} src={user.profileImage} />}</td>
-                  <td>{user.name}</td>
-                  <td>{user.userName}</td>
-                  <td>{user.phone}</td>
-                  <td>{user.collections}</td>
-                  <td>{user.sneakerSize}</td>
-                  <td>{user.favoriteBrands}</td>
-                  <td>{user.sneakerCount}</td>
-                  <td>{user.sneakerScans}</td>
-                  <td>
-                    <Link to={`/users/edit-user/${user.uuid}`}>
-                      <span className="fa fa-edit" aria-hidden="true"></span>
-                    </Link>
-                  </td>
-                  <td>
-                    <span className="fa fa-trash" style={{cursor: 'pointer'}} aria-hidden="true" onClick={() => this.removeUser(user.uuid, index)}></span>
-                  </td>
-                </tr>
-                )) :
-                (
-                  <tr>
-                    <td colSpan="15" className="text-center">{responseMessage}</td>
-                  </tr>
-                )
+                  this.state.users.map((user, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{<img style={{ height: '50px', width: '50px' }} src={user.profileImage} />}</td>
+                      <td>{user.name}</td>
+                      <td>{user.userName}</td>
+                      <td>{user.phone}</td>
+                      <td>{user.collections}</td>
+                      <td>{user.sneakerSize}</td>
+                      <td>{user.favoriteBrands}</td>
+                      <td>{user.sneakerCount}</td>
+                      <td>{user.sneakerScans}</td>
+                      <td>
+                        <Link to={`/users/edit-user/${user.uuid}`}>
+                          <span className="fa fa-edit" aria-hidden="true"></span>
+                        </Link>
+                      </td>
+                      <td>
+                        <span className="fa fa-trash" style={{ cursor: 'pointer' }} aria-hidden="true" onClick={() => this.removeUser(user.uuid, index)}></span>
+                      </td>
+                    </tr>
+                  )) :
+                  (
+                    <tr>
+                      <td colSpan="15" className="text-center">{responseMessage}</td>
+                    </tr>
+                  )
                 }
               </tbody>
             </table>
