d9415261ef4a4873f7ef8348d0b44dc216b78d06
Arslan Sajid <arslansajid951@gmail.com>
2020-05-28 17:35:29 +0500

    get signed in user

--- src/backend/services/authService.js
+++ src/backend/services/authService.js
@@ -58,3 +58,26 @@ export async function getCurrentUserId() {
     return user.uid;
   }
 }
+
+export async function getSignedInUser() {
+  console.log("CALEED")
+  try {
+    await firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        // User is signed in.
+        const res = await user;
+        console.log("HHA  res  HHAHA", res)
+        console.log("HAHAHAHHAHA", user)
+        return res
+      } else {
+        // No user is signed in.
+        console.log("No user is signed in.",)
+        return 0;
+      }
+    })
+  } catch (err) {
+    throw err.message;
+  }
+}
+
+
